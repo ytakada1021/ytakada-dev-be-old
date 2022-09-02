04cbe96826945cb770efffd3764831e7bc3afc22
ytakada <yuki.takada@ytakada.jp>
2022-09-02 12:04:16 +0900

    chore: healthチェックAPIを追加

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function (Request $request) {
-    return "OK";
+Route::get('/health', function (Request $request) {
+    return "health";
 });

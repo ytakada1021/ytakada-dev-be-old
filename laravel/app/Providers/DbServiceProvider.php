c8e7d4c55ab6eb7057b9e1bbc28c417dce2600b6
ytakada <yuki.takada@ytakada.jp>
2022-09-05 18:48:21 +0900

    chore: Doctrineのセットアップ処理を追加

---
+++ laravel/app/Providers/DbServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Providers;
+
+use Doctrine\DBAL\Types\Type;
+use Doctrine\ORM\Configuration;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\ORMSetup;
+use Illuminate\Support\ServiceProvider;
+use Mapping\PostIdType;
+
+class DbServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        // Doctrine EntityManagerをDIコンテナへ登録する
+        // See also:
+        // - https://www.doctrine-project.org/projects/doctrine-orm/en/2.13/reference/configuration.html#obtaining-an-entitymanager
+        // - https://www.doctrine-project.org/projects/doctrine-orm/en/current/reference/xml-mapping.html
+        $this->app->singleton(EntityManagerInterface::class, function (): EntityManager {
+            /** @var Configuration $config */
+            $config = ORMSetup::createXMLMetadataConfiguration(
+                paths: [__DIR__ . '/../../config/mapping'],
+                isDevMode: false
+            );
+
+            return EntityManager::create(
+                connection: [
+                    'driver' => 'pdo_mysql',
+                    'user' => config('database.connections.mysql.username'),
+                    'password' => config('database.connections.mysql.password'),
+                    'host' => config('database.connections.mysql.host'),
+                    'dbname' => config('database.connections.mysql.database')
+                ],
+                config: $config
+            );
+        });
+    }
+
+    public function boot(): void
+    {
+        // Doctrineのカスタムタイプを登録する
+        // See also: https://www.doctrine-project.org/projects/doctrine-orm/en/2.8/cookbook/custom-mapping-types.html
+        Type::addType('post_id', PostIdType::class);
+    }
+}
